9f5cc3490af4086633a13ec7e273504893d0ad4e
tianjing <tian___jing@hotmail.com>
2019-11-24 20:38:00 +0800

    Revert "调度报表优化：新增日志报表的存储表，三天内的任务日志会以每分钟一次的频率异步同步至报表中；任务报表仅读取报表数据，极大提升加载速度；"
    This reverts commit 7ab75aa817c7d9aac4a6dba25418d1273d819dd7.
    
    # Conflicts:
    #	doc/XXL-JOB官方文档.md
    #	xxl-job-admin/src/main/java/com/xxl/job/admin/core/thread/JobLogReportHelper.java
    
    重新调整达梦目录和代码避免和官方版本冲突

---
+++ xxl-job-admin-dm/src/main/java/com/xxl/job/admin/config/ScanBasePackageConfig.java
@@ -0,0 +1,15 @@
+//package com.xxl.job.admin.config;
+//
+//import org.springframework.context.annotation.ComponentScan;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.stereotype.Component;
+//
+///**
+// * @author 田径
+// * @create 2019-11-24 19:48
+// * @desc
+// **/
+//@ComponentScan("com.xxl.job.admin.**")
+//@Configuration
+//public class ScanBasePackageConfig {
+//}
